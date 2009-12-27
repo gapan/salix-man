9c92edeeff2255e22068344e6914fc2f696b9cf2
fredgnix <fredgnix@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2009-12-27 11:32:16 +0000

    fix typo

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 # english man page
 txt2tags -t man -i src/salix.t2t -o src/salix.1
 gzip src/salix.1
-install -D -m 644 src/en/salix.1.gz $DESTDIR/usr/man/man1/salix.1.gz
+install -D -m 644 src/salix.1.gz $DESTDIR/usr/man/man1/salix.1.gz
